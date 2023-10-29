d1c0445e39f91f5596161de49ba633e7877976e3
Sonia Trinidad <142050648+SonyTry@users.noreply.github.com>
2023-10-29 19:42:01 -0300

    validacion

--- js/formulario.js
+++ js/formulario.js
@@ -1,39 +1,65 @@
-// function leeformu(){
-//     var formuDatos = {
-//         apellido: document.getElementById('apellido').value,
-//         nombre: document.getElementById('nombre').value,
-//         domicilio: document.getElementById('domicilio').value,
-//         dni: document.getElementById('dni').value,
-//         email: document.getElementById('email').value,
-//         mensaje: document.getElementById('mensaje').value
+const form = document.getElementsByTagName("form")[0];
+const mail = document.getElementById("mail");
 
+// Validación del e-mail de los formularios
 
-//     };
-// console.log(leeformu);
-// }
+let error = email;
+while ((error = error.nextSibling).nodeType != 1);
 
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+function addEvent(element, event, callback) {
+  let previousEventCallBack = element["on" + event];
+  element["on" + event] = function (e) {
+    const output = callback(e);
 
+    if (output === false) return false;
 
-const apellido = document.querySelector('[name=apellido]');
-const nombre = document.querySelector('[name=nombre]');
-const domicilio = document.querySelector('[name=domicilio]');
-const dni = document.querySelector('[name=dni]');
-const email = document.querySelector('[name=email]');
-const mensaje = document.querySelector('[name=mensaje]');
-
-apellido.addEventListener("blur", function (e){
-    const field =e.target;
-    const fieldValue =e.target.value;
-    if (fieldValue.lenght=== 0){
-        field.classList.add("Invalido");
-        field.nextElementSibling.classList.add("error");
-        field.nextElementSibling.innerText= "Usuario es requerido";
-    } else{
-        field.classList.remove("Invalido");
-        field.nextElementSibling.classList.remove("error");
-        field.nextElementSibling.innerText= "";
-
-
+    if (typeof previousEventCallBack === "function") {
+      output = previousEventCallBack(e);
+      if (output === false) return false;
     }
+  };
 }
+
+// Ahora podemos reconstruir nuestra restricción de validación
+// Debido a que no confiamos en la pseudoclase de CSS, tenemos que
+// establecer explícitamente la clase valid/invalid en nuestro campo de correo electrónico
+addEvent(window, "load", function () {
+  // Aquí probamos si el campo está vacío (recuerda, el campo no es obligatorio)
+  // Si no es así, verificamos si su contenido es una dirección de correo electrónico con el formato correcto.
+  const test = email.value.length === 0 || emailRegExp.test(email.value);
+
+  email.className = test ? "valid" : "invalid";
+});
+
+// Esto define lo que sucede cuando el usuario escribe en el campo
+addEvent(email, "input", function () {
+  const test = email.value.length === 0 || emailRegExp.test(email.value);
+  if (test) {
+    email.className = "valid";
+    error.innerHTML = "";
+    error.className = "error";
+  } else {
+    email.className = "invalid";
+  }
+});
+
+// Esto define lo que sucede cuando el usuario intenta enviar los datos.
+addEvent(form, "submit", function () {
+  const test = email.value.length === 0 || emailRegExp.test(email.value);
+
+  if (!test) {
+    email.className = "invalid";
+    error.innerHTML = "Espero un correo electrónico, querido!";
+    error.className = "error active";
+
+    // Algunos navegadores antiguos no son compatibles con el método event.preventDefault ()
+    return false;
+  } else {
+    email.className = "valid";
+    error.innerHTML = "";
+    error.className = "error";
+  }
+});
