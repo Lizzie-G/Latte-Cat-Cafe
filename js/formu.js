7496a6270fb85972012afa1a45440480b988cb56
NeoMatias <144752940+NeoMatias@users.noreply.github.com>
2023-10-29 00:44:25 -0300

    levanta

--- js/formu.js
+++
@@ -1,8 +0,0 @@
-let apellido =  document.getElementById("apellido");
-let nombre = document.getElementById("nombre");
-let domicilio = document.getElementById("domicilio");
-let dni = document.getElementById("dni");
-let email
-
-
-
